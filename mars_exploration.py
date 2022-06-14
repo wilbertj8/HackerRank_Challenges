7fbace26c9973be62b611b594ff492c6a4309acf
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-06-14 16:08:28 -0400

    Add files via upload

---
+++ mars_exploration.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    count = 0
+    for i in range(0, len(s), 3):
+        count += (s[i] != "S") + (s[i+1] != "O") + (s[i+2] != "S")
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
