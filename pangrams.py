bac6c4ce8d834a47f1b2bfefaa1818805f7e8704
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-06-14 16:07:58 -0400

    Add files via upload

---
+++ pangrams.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+import string
+def pangrams(s):
+    # Write your code here
+    pangram = set(s.lower()) >= set(string.ascii_lowercase)
+    return "pangram" if pangram else "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
