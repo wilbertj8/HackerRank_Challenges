e15ba36453ff612999804c77bb82895573166f0f
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-06-15 17:44:45 -0400

    Add files via upload

---
+++ maximum_perimeter_triangle.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumPerimeterTriangle' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY sticks as parameter.
+#
+
+def maximumPerimeterTriangle(sticks):
+    # Write your code here
+    sticks.sort(reverse = True)
+    for i in range(len(sticks) - 2):
+        if sticks[i+2] + sticks[i+1] > sticks[i]:
+            return sticks[i+2:i-1:-1]
+    return [-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    sticks = list(map(int, input().rstrip().split()))
+
+    result = maximumPerimeterTriangle(sticks)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ migratory_birds.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+from collections import Counter
+def migratoryBirds(arr):
+    # Write your code here
+    counts = Counter(sorted(arr))
+    return counts.most_common(1)[0][0]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
