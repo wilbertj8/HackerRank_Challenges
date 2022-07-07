dc1d6b52452b031f1e75b2161309fae313b331a8
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-07-07 17:08:02 -0400

    Add files via upload

---
+++ sansa_and_xor.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sansaXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+from functools import reduce
+def sansaXor(arr):
+    return reduce(lambda x, y : x ^ y, arr[::2]) if len(arr) % 2 else 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = sansaXor(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sansaXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+from functools import reduce
+def sansaXor(arr):
+    return reduce(lambda x, y : x ^ y, arr[::2]) if len(arr) % 2 else 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = sansaXor(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
