d1df0fe19ee6f3cd0722f5dd0668a79fcfebb196
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-07-07 16:44:21 -0400

    Add files via upload

---
+++ max_min.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    arr.sort()
+    unfairness = math.inf
+    for i in range(0, len(arr) - k + 1):
+        unfairness = min(unfairness, abs(arr[i] - arr[i + k - 1]))
+    return unfairness
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
