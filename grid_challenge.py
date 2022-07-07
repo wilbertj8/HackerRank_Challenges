6957d3373d88470433bc76c46e2c2940e5a18e8c
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-07-07 17:01:54 -0400

    Add files via upload

---
+++ grid_challenge.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    grid = list(map(sorted, grid))
+    for j in range(len(grid[0])):
+        for i in range(1, len(grid)):
+            if ord(grid[i - 1][j]) > ord(grid[i][j]):
+                return "NO"
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
