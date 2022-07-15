a2e0aa74ff3aaee4e7c5a36e09d03458e03f934e
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-07-15 15:57:48 -0400

    Add files via upload

---
+++ counter_game.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'counterGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def counterGame(n):
+    ones = format(n, 'b').count('1')
+    rightzeros = 0
+    while not n & 1:
+        n >>= 1
+        rightzeros += 1
+    return 'Richard' if rightzeros + ones & 1 else 'Louise'
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = counterGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
