553a45bc4a5e44015c21f08ef539dea8793cc6c2
Wilbert Joseph <52422676+wilbertj8@users.noreply.github.com>
2022-07-15 15:31:53 -0400

    Add files via upload

---
+++ gaming_array_1.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gamingArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def gamingArray(arr):
+    count, mx = 0, 0
+    for i, val in enumerate(arr):
+        if val > mx:
+            mx = val
+            count += 1
+    return "BOB" if count & 1 else "ANDY"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input().strip())
+
+    for g_itr in range(g):
+        arr_count = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = gamingArray(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
